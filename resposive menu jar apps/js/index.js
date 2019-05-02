251cf802cfda80cd7ee61d1b2afbd7de2447a27b
razar3gtd <44323583+razar3gtd@users.noreply.github.com>
2019-05-02 23:09:11 +0200

    Add files via upload

--- resposive menu jar apps/js/index.js
+++ resposive menu jar apps/js/index.js
@@ -43,3 +43,92 @@ $(document).ready(function () {
 			}
 		});
 	}
+
+
+
+
+
+const BREAKPOINTY = {
+	XS:599,
+	LT:750,
+	GT:956,
+	XL:1050,
+}
+
+var ILOŚĆ_DODAWANYCH_KLOCKÓW = 0;
+var ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW = 0;
+
+// PO zmianie wielkości okna
+$(window).resize((ev)=>{
+	console.log(ev);
+	$(`div.projekt:lt(${ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW})`).show();
+
+	if($(window).width() < BREAKPOINTY.XS
+		){
+		ILOŚĆ_DODAWANYCH_KLOCKÓW = 1;
+		ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW = 3;
+	}
+	else if($(window).width() < BREAKPOINTY.LT ){
+		ILOŚĆ_DODAWANYCH_KLOCKÓW = 2;
+		ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW = 6;
+	}
+	else if($(window).width() < BREAKPOINTY.GT){
+		ILOŚĆ_DODAWANYCH_KLOCKÓW = 3;
+		ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW = 9;
+	}
+	else if($(window).width() < BREAKPOINTY.XL){
+		ILOŚĆ_DODAWANYCH_KLOCKÓW = 4;
+		ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW = 12;
+	}
+	else if($(window).width() > BREAKPOINTY.XL){
+		ILOŚĆ_DODAWANYCH_KLOCKÓW = 4;
+		ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW = 12;
+	}
+	$(`div.projekt:lt(${ILOŚĆ_WSZYSTKICH_KLOCKÓW})`).hide();
+	$(`div.projekt:lt(${ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW})`).show();
+})
+
+
+    $(document).ready(function () {
+    ILOŚĆ_WSZYSTKICH_KLOCKÓW = $("div.projekt").size();
+
+	if ( $(window).width() < BREAKPOINTY.XS ){
+        ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW = 1;
+    } else if ($(window).width() <= BREAKPOINTY.LT ) {
+        ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW = 6;
+    }
+    else if ($(window).width() <= BREAKPOINTY.GT) {
+        ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW = 9;
+    }
+    else if ($(window).width() > BREAKPOINTY.GT) {
+        ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW = 12;
+    }
+
+   if($(window).width() < BREAKPOINTY.XS ){
+		ILOŚĆ_DODAWANYCH_KLOCKÓW = 1;
+	}
+	else if($(window).width() < BREAKPOINTY.LT ){
+		ILOŚĆ_DODAWANYCH_KLOCKÓW = 2;
+	}
+	else if($(window).width() < BREAKPOINTY.GT){
+		ILOŚĆ_DODAWANYCH_KLOCKÓW = 3;
+	}
+	else if($(window).width() > BREAKPOINTY.GT){
+		ILOŚĆ_DODAWANYCH_KLOCKÓW = 4;
+	}
+
+    
+
+    $(`div.projekt:lt(${ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW})`).show();
+    
+    $('div .batton3').click(function () {
+    	if(ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW + ILOŚĆ_DODAWANYCH_KLOCKÓW <= ILOŚĆ_WSZYSTKICH_KLOCKÓW){
+			ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW += ILOŚĆ_DODAWANYCH_KLOCKÓW;
+		
+		}
+		else {
+		ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW = ILOŚĆ_WSZYSTKICH_KLOCKÓW;
+		}
+        $('div.projekt:lt('+ILOŚĆ_WYŚWIETLANYCH_KLOCKÓW+')').show();
+    });
+});
